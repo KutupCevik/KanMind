a7b99edfe2eb6fcdb50ed715c1ca4cffd7bea98f
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-15 12:55:26 +0200

    Update Board fields

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Board(models.Model):
     title = models.CharField(max_length=255)
-    owner = models.ForeignKey()
-    members = models.ManyToManyField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_boards')
+    members = models.ManyToManyField(User, through='BoardMember', related_name='boards')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -46,6 +46,7 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Comment(models.Model):
     task = models.ForeignKey()
