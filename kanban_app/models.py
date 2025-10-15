8ec836b0dd56e51f138333bfe1a5731c613a6436
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-15 13:20:57 +0200

    Update Task fields

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -36,17 +36,17 @@ class Task(models.Model):
         ('high', 'High'),
     ]
 
-    bord = models.ForeignKey()
+    bord = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    status =models.CharField(max_length=20)
-    priority = models.CharField(max_length=20)
-    created_by = models.ForeignKey()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    due_date = models.DateField()
-    assignee = models.ForeignKey()
-    reviewer = models.ForeignKey()
+    status =models.CharField(max_length=20, choices=STATUS_CHOICES, default='to-do')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='medium')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tasks')
+    created_at = models.DateTimeField(auto_now_add=True)    #auto_now_add=True: Wird nur einmal beim Erstellen gesetzt
+    updated_at = models.DateTimeField(auto_now=True)    #auto_now=True: Wird bei jedem Speichern automatisch aktualisiert
+    due_date = models.DateField(null=True, blank=True)  #null=True: das Feld darf in der Datenbank leer bleiben und blank=True: Im Formular darf das Feld leer übermittelt werden
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tasks') #wenn User gelöscht wird, assignee = NULL
+    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='review_tasks')
 
     def __str__(self):
         return self.title
