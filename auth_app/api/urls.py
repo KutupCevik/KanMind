9ff30801812bfa0aaead15a941bb4b8e69b59548
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-17 12:17:55 +0200

    Add LoginView

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from auth_app.api.views import RegistrationView
+from auth_app.api.views import RegistrationView, LoginView, CustomLoginView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
