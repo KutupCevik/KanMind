7ec021b5e367d93deaa4fee4fe31e7e49792c7fe
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-17 07:58:18 +0200

    Add RegistrationView

---
+++ auth_app/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
+from auth_app.api.serializers import RegistrationSerializer
+
+
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key,
+                "fullname": user.first_name,
+                "email": user.email,
+                "user_id": user.id
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
