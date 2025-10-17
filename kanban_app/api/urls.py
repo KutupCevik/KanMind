de19dd469f5d3e87b6fbe44bb5b49b59d6f529e7
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-17 15:59:08 +0200

    Add BoardDetailView

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from kanban_app.api.views.board import BoardListCreateView
+from kanban_app.api.views.board import BoardListCreateView, BoardDetailView
 
 urlpatterns = [
     path('boards/', BoardListCreateView.as_view(), name='board-list'),
+    path('boards/<int:pk>/', BoardDetailView.as_view(), name='board-detail'),
 ]
