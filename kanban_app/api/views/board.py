d6247e74a59762b8a362630e99ca571f1f7ce78f
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-17 15:48:19 +0200

    Update BoardListCreateView

--- kanban_app/api/views/board.py
+++ kanban_app/api/views/board.py
@@ -13,11 +13,9 @@ POST /api/boards/: Erstellt ein neues Board, setzt den Benutzer als Owner.
 class BoardListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         user = self.request.user
-        return (
-            Board.objects.filter(members=user)
-            .union(Board.objects.filter(owner=user))    #https://docs.djangoproject.com/en/5.2/ref/models/querysets/#union
-            .distinct() #https://docs.djangoproject.com/en/5.2/ref/models/querysets/#distinct
-        )
+        owned = Board.objects.filter(owner=user)
+        member = Board.objects.filter(members=user)
+        return (owned | member).distinct()
 
     def get_serializer_class(self):
         return BoardCreateSerializer if self.request.method == 'POST' else BoardListSerializer
