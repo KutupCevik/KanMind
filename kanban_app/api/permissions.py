e8d5cba222f386dabd89f30e6a265f346155ff7b
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-18 13:50:57 +0200

    Add IsBoardOwner

--- kanban_app/api/permissions.py
+++ kanban_app/api/permissions.py
@@ -1,10 +1,20 @@
 from rest_framework.permissions import BasePermission
 
 
-# Zugriff auf Boards wenn der Benutzer Mitglied oder Owner ist.
 class IsBoardMemberOrOwner(BasePermission):
+    """
+    Zugriff auf Boards wenn der Benutzer Mitglied oder Owner ist
+    """
     def has_object_permission(self, request, view, obj):
         user = request.user
         return bool(
             user == obj.owner or obj.members.filter(id=user.id).exists()
         )
+
+
+class IsBoardOwner(BasePermission):
+    '''
+    Zugriff nur für den Owner eines Boards
+    '''
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
