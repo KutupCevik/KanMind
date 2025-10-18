08036c363228bf89a39a0a9c4b2582c90ff0b907
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-18 14:13:30 +0200

    Update BoardDetailUpdateDeleteView

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from kanban_app.api.views.board import BoardListCreateView, BoardDetailUpdateView
+from kanban_app.api.views.board import BoardListCreateView, BoardDetailUpdateDeleteView
 
 urlpatterns = [
     path('boards/', BoardListCreateView.as_view(), name='board-list'),
-    path('boards/<int:pk>/', BoardDetailUpdateView.as_view(), name='board-detail'),
+    path('boards/<int:pk>/', BoardDetailUpdateDeleteView.as_view(), name='board-detail'),
 ]
