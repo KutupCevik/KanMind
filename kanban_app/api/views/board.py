c0dc97dc3d0c843f6cf2999d141c9b5260fd7ac1
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-18 12:43:30 +0200

    Update BoardListCreateView

--- kanban_app/api/views/board.py
+++ kanban_app/api/views/board.py
@@ -22,24 +22,10 @@ class BoardListCreateView(generics.ListCreateAPIView):
 
     def get_serializer_class(self):
         return BoardCreateSerializer if self.request.method == 'POST' else BoardListSerializer
-
-    def get_serializer_context(self):
-        serializer_context = super().get_serializer_context()
-        serializer_context['request'] = self.request
-        return serializer_context
     
     def perform_create(self, serializer):
         # Owner automatisch aus dem eingeloggten Benutzer setzen
         serializer.save(owner=self.request.user)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        board = serializer.instance
-        response_serializer = BoardListSerializer(board)
-
-        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
 
 '''
