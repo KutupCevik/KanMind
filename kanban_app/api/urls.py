0ac09e75ebb684a92ae6350c9b341e420229a2af
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-20 12:06:50 +0200

    Add TaskCreateView

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from kanban_app.api.views.board import BoardListCreateView, BoardDetailUpdateDeleteView
+from kanban_app.api.views.task import TaskCreateView
 
 urlpatterns = [
     path('boards/', BoardListCreateView.as_view(), name='board-list'),
     path('boards/<int:pk>/', BoardDetailUpdateDeleteView.as_view(), name='board-detail'),
+    path('tasks/', TaskCreateView.as_view(), name='task-create'),
 ]
