13cd737a65bc11476e572943a381019c0a6bb4d6
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-21 11:11:54 +0200

    Add CommentSerializer

---
+++ kanban_app/api/serializers/comment.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from kanban_app.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    '''Serializer für Kommentare (GET / POST).'''
+    author = serializers.CharField(source='author.first_name', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'created_at', 'author', 'content']
+        read_only_fields = ['id', 'created_at', 'author']
