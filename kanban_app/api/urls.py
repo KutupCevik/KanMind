56fc48b1aa445b1dffb7bf17edc565bb5ccde978
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-21 12:56:51 +0200

    Add TasksAssignedToMeView

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from kanban_app.api.views.board import BoardListCreateView, BoardDetailUpdateDeleteView
-from kanban_app.api.views.task import TaskCreateView, TaskUpdateDeleteView
+from kanban_app.api.views.task import TaskCreateView, TaskUpdateDeleteView, TasksAssignedToMeView
 from kanban_app.api.views.comment import CommentListCreateView, CommentDeleteView
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('boards/<int:pk>/', BoardDetailUpdateDeleteView.as_view(), name='board-detail'),
     path('tasks/', TaskCreateView.as_view(), name='task-create'),
     path('tasks/<int:pk>/', TaskUpdateDeleteView.as_view(), name='task-update-delete'),
+    path('tasks/assigned-to-me/', TasksAssignedToMeView.as_view(), name='tasks-assigned-to-me'),
     path('tasks/<int:task_id>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
     path('tasks/<int:task_id>/comments/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
