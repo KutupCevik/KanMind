50778e1761a4fc730f605dc391f7987d94aa5e84
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-22 10:28:39 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / os.getenv('DATABASE_NAME', 'db.sqlite3'),
+        'NAME': BASE_DIR / os.getenv('DATABASE_NAME'),
     }
 }
 
