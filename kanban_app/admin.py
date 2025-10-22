a2f33e89d827f2513e04d6c5d042e3cb20fc0c52
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2025-10-22 15:51:33 +0200

    Add BoardAdmin

--- kanban_app/admin.py
+++ kanban_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Board
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'created_at')
+    search_fields = ('title', 'owner__username', 'owner__email')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
